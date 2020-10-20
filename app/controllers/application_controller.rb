8c78d725b083316745a2a720c74eda45dfbe4ba9
Yuna Shimizu <h.yuina627@gmail.com>
2020-10-20 16:49:43 -0700

    Create introduce column to Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = %i[email name password password_confirmation zipcode address]
+    added_attrs = %i[email name password password_confirmation zipcode address introduce]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
