c6b32cf2189d3cb09e4b1c8256c46d7f5e450aa7
Yuna Shimizu <h.yuina627@gmail.com>
2020-10-22 16:00:11 -0700

    Delete validation of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
-  validates :email, length: { maximum: 50 }
 
   has_many :books
 end
