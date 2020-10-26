b315fa0b36ba80a625e9e7b60afd9b88d25fbf2f
Yuna Shimizu <h.yuina627@gmail.com>
2020-10-25 22:52:38 -0700

    Redirect root to books_index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def index
+    redirect_to books_url
+  end
+
   protected
 
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'application#index'
   devise_for :users
   resources :books
-  get '/users/:id', to: 'users#show'
+  get '/users/:id' => 'users#show', as: :user
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
