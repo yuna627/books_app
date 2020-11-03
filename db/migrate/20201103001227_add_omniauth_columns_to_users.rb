efb122736e32be6d4aadc6a98b4fe998a7465485
Yuna Shimizu <h.yuina627@gmail.com>
2020-11-02 18:24:53 -0800

    Add validation to omniauth columns

--- db/migrate/20201103001227_add_omniauth_columns_to_users.rb
+++ db/migrate/20201103001227_add_omniauth_columns_to_users.rb
@@ -1,6 +1,8 @@
 class AddOmniauthColumnsToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :provider, :string
-    add_column :users, :uid, :string
+    add_column :users, :provider, :string, null: false, default: ''
+    add_column :users, :uid, :string, null: false, default: ''
+
+    add_index :users, %i[uid provider], unique: true
   end
 end
