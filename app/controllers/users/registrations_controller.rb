f83e7df63a80e9dfcc83967e6ffbff0472be1227
Yuna Shimizu <h.yuina627@gmail.com>
2020-11-04 11:09:17 -0800

    Apply rubocop-fjord to add frozen_string_literal# Please enter the commit message for your changes. Lines starting

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Users::RegistrationsController < Devise::RegistrationsController
   def build_resource(hash = {})
     hash[:uid] = User.create_unique_string
--- db/migrate/20201103001227_add_omniauth_columns_to_users.rb
+++ db/migrate/20201103001227_add_omniauth_columns_to_users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddOmniauthColumnsToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :provider, :string, null: false, default: ''
