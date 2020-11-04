3130b96f2458251050f2febd951e9ecb20511850
Yuna Shimizu <h.yuina627@gmail.com>
2020-11-03 23:56:25 -0800

    Create self.create_unique_string and self.find_for_github_oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,19 @@ class User < ApplicationRecord
   validates :introduction, length: { maximum: 100 }
   validates :zipcode, format: { with: /\A\d{3}[-]\d{4}\z/ }, allow_blank: true
 
-  has_many :books, dependent: :destroy
+  def self.create_unique_string
+    SecureRandom.uuid
+  end
+
+  def self.find_for_github_oauth(auth, _signed_in_resource = nil)
+    user = User.find_by(provider: auth.provider, uid: auth.uid)
+
+    user ||= User.new(provider: auth.provider,
+                      uid: auth.uid,
+                      name: auth.info.name,
+                      email: auth.info.email,
+                      password: Devise.friendly_token[0, 20])
+    user.save
+    user
+  end
 end
