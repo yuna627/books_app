107982c0268d9523f0dec47a16caae50e3c0daa2
Yuna Shimizu <h.yuina627@gmail.com>
2020-11-03 23:58:56 -0800

    Add registrations and omniauth_callbacks routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'application#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   resources :users, only: :show
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
