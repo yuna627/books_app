091ef290caffc7361bf12ba1b386021c504b494d
Yuna Shimizu <h.yuina627@gmail.com>
2020-11-06 00:16:53 -0800

    Add :avatar as a parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit :sign_up, keys: %i[name email password password_confirmation]
-    devise_parameter_sanitizer.permit :account_update, keys: %i[name email password password_confirmation zipcode address introduction]
+    devise_parameter_sanitizer.permit :account_update, keys: %i[name email password password_confirmation zipcode address introduction avatar]
     devise_parameter_sanitizer.permit :sign_in, keys: %i[email password]
   end
 end
