46572bd32419d5a1bdcc9d40d25a1262d90d51d6
Yuna Shimizu <h.yuina627@gmail.com>
2020-11-08 20:07:44 -0800

    Delete unnecessary comments from user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@
 class User < ApplicationRecord
   has_many :books, dependent: :destroy
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[github]
