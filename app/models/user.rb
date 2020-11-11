e42cbadaec4d5922a5e72342fcd4e795b890cc43
Yuna Shimizu <h.yuina627@gmail.com>
2020-11-11 00:32:38 -0800

    Create three methods (follow,folloed_id,following?)

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,4 +32,16 @@ class User < ApplicationRecord
     user.save
     user
   end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
