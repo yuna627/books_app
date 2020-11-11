9a279aa4781c24f8bebc23ff420013ebf4743a13
Yuna Shimizu <h.yuina627@gmail.com>
2020-11-11 00:28:51 -0800

    Add active_relationships to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@
 class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_one_attached :avatar
+  has_many :active_relationships, class_name: 'Relationship',
+                                  foreign_key: 'follower_id',
+                                  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
