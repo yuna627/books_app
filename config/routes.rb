b33e02123e997e12bf2def2e0ba397955976b272
Yuna Shimizu <h.yuina627@gmail.com>
2020-11-11 23:41:27 -0800

    Add routes such as  follow,follower,followings,and followers

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,16 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
+
+  post 'follow/:id' => 'relationships#follow', as: 'follow'
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
+
+  resources :users do
+    member do
+      get :followings, :followers
+    end
+  end
+
   resources :users, only: :show
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
