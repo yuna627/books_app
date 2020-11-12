874d58917a2f086d09a1612cfcbd55630640d79a
Yuna Shimizu <h.yuina627@gmail.com>
2020-11-11 23:59:07 -0800

    Add follow and unfollow actions to RelationshipsController

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class RelationshipsController < ApplicationController
+  def follow
+    current_user.follow(params[:id])
+    redirect_to root_path
+  end
+
+  def unfollow
+    current_user.unfollow(params[:id])
+    redirect_to root_path
+  end
+end
