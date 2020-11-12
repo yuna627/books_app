ca556bad5a5dbe5058d69af5effb35c0d9bf847d
Yuna Shimizu <h.yuina627@gmail.com>
2020-11-11 23:38:10 -0800

    Add follow and unfollow actions to User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def followings
+    @user = User.find(params[:id])
+  end
+
+  def followers
+    @user = User.find(params[:id])
+  end
 end
