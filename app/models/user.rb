8b274dfa8993e194c242d5d4d12575a9cae42280
Yuna Shimizu <h.yuina627@gmail.com>
2020-11-11 23:32:21 -0800

    Change some variable names

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@
 class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_one_attached :avatar
-  has_many :active_relationships, class_name: 'Relationship',
-                                  foreign_key: 'follower_id',
-                                  dependent: :destroy
-  has_many :passive_relationships, class_name: 'Relationship',
-                                   foreign_key: 'followed_id',
-                                   dependent: :destroy
-  has_many :following, through: :active_relationships, source: :followed
-  has_many :followers, through: :passive_relationships, source: :follower
+  has_many :follower, class_name: 'Relationship',
+                      foreign_key: 'follower_id',
+                      dependent: :destroy
+  has_many :followed, class_name: 'Relationship',
+                      foreign_key: 'followed_id',
+                      dependent: :destroy
+  has_many :following_users, through: :follower, source: :followed
+  has_many :follower_users, through: :followed, source: :follower
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
