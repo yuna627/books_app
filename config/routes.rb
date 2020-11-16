bc092ecdc6604034bc4a7a3df7f45f331c6af06e
Yuna Shimizu <h.yuina627@gmail.com>
2020-11-16 00:06:15 -0800

    Add :reports to routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
 
   resources :users, only: :show
   resources :books
+  resources :reports
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
