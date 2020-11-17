01aa972708177b726d404e22c687550a1c9c6fe2
Yuna Shimizu <h.yuina627@gmail.com>
2020-11-16 23:03:29 -0800

    Add comments table to schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_022416) do
+ActiveRecord::Schema.define(version: 2020_11_17_064248) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +41,12 @@ ActiveRecord::Schema.define(version: 2020_11_16_022416) do
     t.string "author"
     t.string "picture"
     t.integer "user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "relationships", force: :cascade do |t|
