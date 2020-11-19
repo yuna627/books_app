204df5a469df8ceb4805d8961965690ac2b51aa8
Yuna Shimizu <h.yuina627@gmail.com>
2020-11-18 16:11:06 -0800

    Add validation of null:false to follower_id and followed_id

--- db/migrate/20201106221457_create_relationships.rb
+++ db/migrate/20201106221457_create_relationships.rb
@@ -3,8 +3,8 @@
 class CreateRelationships < ActiveRecord::Migration[6.0]
   def change
     create_table :relationships do |t|
-      t.integer :follower_id
-      t.integer :followed_id
+      t.integer :follower_id, null: false
+      t.integer :followed_id, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -44,8 +44,8 @@ ActiveRecord::Schema.define(version: 2020_11_06_221457) do
   end
 
   create_table "relationships", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
