ba2563826ddb5ca556beb053a318d870d597e87d
Yuna Shimizu <h.yuina627@gmail.com>
2020-11-19 13:25:59 -0800

    Refactor routes post to resources

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,9 @@
 class UsersController < ApplicationController
   before_action :set_user
 
-  def show; end
+  def show
+    @relationship = @user.followed.find_by(follower_id: current_user.id)
+  end
 
   def followings
     @followings = @user.following_users
--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,13 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
-  post 'follow/:id' => 'relationships#follow', as: 'follow'
-  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
-
   resources :users do
     member do
       get :followings, :followers
     end
   end
 
+  resources :relationships, only: %i[create destroy]
   resources :users, only: :show
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
