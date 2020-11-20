e7b9951b6c9039fdd383da94837d19ee7cbb7eaa
Yuna Shimizu <h.yuina627@gmail.com>
2020-11-19 22:32:07 -0800

    Fix methods for when the IDs doesn't exist

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,12 +2,16 @@
 
 class RelationshipsController < ApplicationController
   def create
-    current_user.follow(params[:follow_id])
+    user = User.find_by_id(params[:follow_id])
+    if user
+      current_user.follow(params[:follow_id]) unless current_user.following?(user)
+    end
     redirect_to root_path
   end
 
   def destroy
-    Relationship.find(params[:id]).destroy
+    relationship = Relationship.find_by_id(params[:id])
+    relationship&.destroy
     redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -43,10 +43,6 @@ class User < ApplicationRecord
     followers.create(followed_id: user_id)
   end
 
-  def unfollow(user_id)
-    follower.find_by(followed_id: user_id).destroy
-  end
-
   def following?(user)
     following_users.include?(user)
   end
